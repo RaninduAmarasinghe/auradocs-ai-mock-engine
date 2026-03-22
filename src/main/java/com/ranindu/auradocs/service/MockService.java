e62d7a35dc50118ca127adaaaa3566400312f482
Ranindu Amarasinghe <raninduamarasinghe@gmail.com>
2026-03-22 18:26:01 +0530

    update ollama req prompt in service layer

--- src/main/java/com/ranindu/auradocs/service/MockService.java
+++ src/main/java/com/ranindu/auradocs/service/MockService.java
@@ -2,16 +2,23 @@ package com.ranindu.auradocs.service;
 
 import com.ranindu.auradocs.model.entity.ApiSpec;
 import com.ranindu.auradocs.repository.ApiSpecRepository;
+import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MockService {
     private final ApiSpecRepository repository;
-
-    public MockService(ApiSpecRepository repository) {
+    private final OllamaChatModel chatModel;
+    public MockService(ApiSpecRepository repository, OllamaChatModel chatModel) {
         this.repository = repository;
+        this.chatModel = chatModel;
     }
-    public ApiSpec createSpec(ApiSpec spec) {
-        return repository.save(spec);
+    public String generateMock(String id) {
+        ApiSpec spec = repository.findById(id).orElseThrow(() -> new RuntimeException("Spec not found"));
+
+        String prompt = "Generate one JSON object with these fields: " + spec.getFields() +
+                ". Context: " + spec.getMockContext() +
+                ". Return ONLY valid JSON.";
+        return chatModel.call(prompt);
     }
 }
